ee1de6255303c2ffe5613d1a59d0539ea673e998
Tim Graupmann <tim@tagenigma.com>
2016-10-04 09:56:04 -0700

    Updated to Razer Store SDK 2.0.175

---
+++ copy_aar.cmd
@@ -0,0 +1,5 @@
+ECHO ON
+COPY /Y store-sdk-standard-release.aar AARRazerSDK\pluginrazersdk\libs
+COPY /Y store-sdk-standard-release.aar InAppPurchases\android\libs
+COPY /Y store-sdk-standard-release.jar InAppPurchases\android\libs
+PAUSE
